215a4bc2f6685aff02114319f83e165b79f6127f
Sarah Dobson <sld21@ic.ac.uk>
2021-10-17 17:44:52 +0100

    added code to account for no parameter provided

--- week1/code/CountLines.sh
+++ week1/code/CountLines.sh
@@ -6,6 +6,25 @@
 #arguments: 1 -> file with lines to be counted
 #date: 7 Oct 2021
 
+
+#checking if input file has been provided
+
+if [ $# -eq 1 ]; then
+
+echo "Input file has been provided"
+
+
+#if no input file has been given, it will provide the following statment and then exit
+
+else 
+
+echo "Cannot complete task; either parameters are missing or incorrectly spelled. Please provide a .csv file"
+
+exit 
+
+fi
+
+#Counting the number of lines in the file
 echo "Counting the number of lines within the file"
 
 NumLines=`wc -l < $1`
