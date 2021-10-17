5f67cbb1ee9eb380d5364949c5c78efcfa8ac0f5
Sarah Dobson <sld21@ic.ac.uk>
2021-10-17 17:34:35 +0100

    tweaked the code

--- week1/code/tiff2png.sh
+++ week1/code/tiff2png.sh
@@ -7,14 +7,47 @@
 #Arguments: f-> .tiff file 
 #Date 8 Oct 2021
 
-for f in *.tif; 
+
+#check if a file has been provided
+if [ $# -eq 0 ]; then 
+
+echo "no input file provided; please provide a .tif file"
+
+exit
+
+else 
+
+echo "input file provided"
+fi
+
+
+
+#now to check if a .csv file has been provided, if not, the process will end.
+echo "checking if .tif file has been provided"
+
+if [[ $1 = *.tif ]] 
+then 
+
+echo "Correct file type"
+else 
+
+echo "wrong file type"
+
+exit
+
+fi
+
+#now to cpnvert .tif file to .png
+
+for f in $1; 
     do  
         echo "Converting $f"; 
         convert "$f"  "$(basename "$f" .tif).png"; 
     done
 
-     
+echo "done!"
 
+exit
 
 
 
