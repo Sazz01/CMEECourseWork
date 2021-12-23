94b7ac5c990b2cc4cf55acb20d9459a531e55e2e
Sarah Dobson <sld21@ic.ac.uk>
2021-12-23 20:32:05 +0000

    added project proposal

---
+++ proposal/CompileLaTeX.sh
@@ -0,0 +1,53 @@
+#/bin/bash
+
+#author: Sarah Dobson sld21@ic.ac.uk
+#script: CompileLaTeX.sh
+#description: script converts latex files into pdfs by compiling latex files with bibtex files, removes excess files and then moves the pdf file to the results folder
+#Arguments: 1 ->  file with extension; Var -> file with exentsion removed
+#Date 7 Oct 2021
+
+
+
+if [ $# -eq 1 ]; then
+
+#now to remove the exenstion on the file being turned into a pdf (if its present, will not change anything if its not)
+
+echo "removing extension from $Var"
+
+Var=$(basename "$1" | cut -d. -f1)
+
+
+
+#converting file to pdf format
+
+echo "converting $Var to pdf "
+
+pdflatex $Var.tex
+bibtex $Var
+pdflatex $Var.tex
+pdflatex $Var.tex
+
+
+
+##Now to cleanup incompelete files 
+rm *.aux
+rm *.log
+rm *.bbl
+rm *.blg
+
+
+#Opening pdf file
+
+echo "opening pdf file"
+
+evince $Var.pdf&
+
+exit
+
+else
+
+ echo "Cannot complete task. Input file is either missing or incorrectly spelled"
+
+exit
+
+fi
